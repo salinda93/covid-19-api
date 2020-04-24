af7b01a976fedf05027341bb4f78bb3ca3845fc9
Salinda <salinda.jayasundara3010@gmail,com>
2020-04-24 19:47:15 +0530

    Display all the data by ewch country

--- covid-19-api/src/App.js
+++ covid-19-api/src/App.js
@@ -3,6 +3,12 @@ import Axios from "axios";
 import "./style.css";
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.getCountryData = this.getCountryData.bind(this);
+    }
+
     state = {
         confirmed :0,
         recovered :0,
@@ -19,7 +25,11 @@ export default class App extends React.Component{
     async getData(){
         const resApi = await Axios.get("https://covid19.mathdro.id/api");
         const resCountries = await Axios.get("https://covid19.mathdro.id/api/countries");
-        const countries = Object.keys(resCountries.data.countries);
+        const countries = [];
+        for(var i = 0; i < resCountries.data.countries.length; i++){
+            countries.push(resCountries.data.countries[i].name);
+        }
+        //const countries = Object.keys(resCountries.data.countries);
         this.setState({
             confirmed:resApi.data.confirmed.value,
             recovered:resApi.data.recovered.value,
@@ -28,11 +38,20 @@ export default class App extends React.Component{
         });
     }
 
+    async getCountryData(e){
+        const res = await Axios.get(`https://covid19.mathdro.id/api/countries/${e.target.value}`);
+        this.setState({
+            confirmed:res.data.confirmed.value,
+            recovered:res.data.recovered.value,
+            deaths:res.data.deaths.value,
+        })
+    }
+
    
 
     renderCountryOptions(){
-        return this.state.countries.map((country, j) =>{
-        return <option key = {j}>{country}</option>
+        return this.state.countries.map(( country, j) =>{
+        return <option key={j}>{ country }</option>
         });
 
     }
@@ -42,7 +61,7 @@ export default class App extends React.Component{
         <div className="container">
             <h1>Covid-19 Update</h1>
 
-            <select>
+            <select onChange={this.getCountryData}>
                 {this.renderCountryOptions()}
             </select>
 
